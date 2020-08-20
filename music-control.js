49f3c59c5f5ca48603dda751000d73b13a45f3e1
moonee <uuuu118@naver.com>
2020-08-20 21:04:42 +0900

    Add timer in player - player 내부에 현재 시간, 전체 음악 시간 삽입함

--- music-control.js
+++ music-control.js
@@ -4,6 +4,10 @@ const music__container = document.getElementById('music__container');
 const playBtn = document.getElementById('play');
 const progress__container = document.getElementById('progress');
 const progress__bar = document.getElementById('progress__bar');
+const current_time = document.getElementById('current_time');
+const whole_time = document.getElementById('whole_time');
+
+
 // file 업로드 시, music controller 보여주기 
 file.onchange = function(){
     let files = this.files;
@@ -15,7 +19,11 @@ file.onchange = function(){
     }
     audio__.src= URL.createObjectURL(files[0]);
     audio__.load();
+    musicControl(); // 자동 재생 
     music__container.classList.add('show');
+    audio__.onloadedmetadata=function(){
+        setTime(audio__.duration);
+    }
 } 
 
 const musicControl = function(){
@@ -35,14 +43,37 @@ const updateProgress=function(e){
     const progressPercent = (currentTime/duration)*100;
     progress__bar.style.width=`${progressPercent}%`;
     //width 업데이트    
+
+    // 현재 시각 구하기 
+    let currentSec = Math.floor(currentTime)%60;
+    let currentMin = Math.floor((currentTime-currentSec)/60);
+    let fill='';
+    if(currentSec.toString().length === 1){
+        fill='0';
+    }
+    current_time.innerText=`${currentMin}:${fill}${currentSec}`;
+
 }
 
+// prgoress 조정 시 해당 시간으로 넘겨주기 
 const setProgress=function(e){
     const width=  this.clientWidth; // progress bar 전체 width
     const clickX= e.offsetX; // progress bar에서 눌린 위치
     const nowTime = (clickX/width)*audio__.duration;
     audio__.currentTime=nowTime;
 }
+
+// 전체 time 지정 
+const setTime=function(duration){
+    const wholeMin = Math.floor(duration/60);
+    const wholeSec = Math.floor(duration)%60;
+    let fill='';
+    if(wholeSec.toString().length===1){
+        fill='0';
+    }
+    whole_time.innerText=`${wholeMin}:${fill}${wholeSec}`;
+}
+
 
 audio__.addEventListener('timeupdate',updateProgress);
 playBtn.addEventListener('click',musicControl);
--- visualizer.js
+++ visualizer.js
@@ -1,7 +1,7 @@
 const audio = document.getElementById('audio');
-let context = new AudioContext();
-let src = context.createMediaElementSource(audio);
-let analyser = context.createAnalyser();
+const context = new AudioContext();
+const src = context.createMediaElementSource(audio);
+const analyser = context.createAnalyser();
 
 let canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
@@ -12,11 +12,10 @@ src.connect(analyser);
 analyser.connect(context.destination);
 
 analyser.fftSize=256;
-let bufferLength = analyser.frequencyBinCount;
-let dataArray=new Uint8Array(bufferLength);
-console.log(dataArray);
-let WIDTH = canvas.width;
-let HEIGHT = canvas.height;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray=new Uint8Array(bufferLength);
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
 
 let lineHeight;
 let x = 0;
@@ -26,22 +25,15 @@ function renderFrame(){
     requestAnimationFrame(renderFrame);
     x = 200;
     analyser.getByteFrequencyData(dataArray);
-    let grd = ctx.createLinearGradient(0, 0, 1700, 0);
+    const grd = ctx.createLinearGradient(0, 0, 1700, 0);
     grd.addColorStop(0, "#7204b1");
     grd.addColorStop(1, "#c483ea");
     ctx.fillRect(0,0,WIDTH,HEIGHT);
     ctx.fillStyle = grd;
     ctx.globalAlpha = 0.5;
-    ctx.beginPath();
-    ctx.moveTo(200, 400);
-    ctx.lineTo(1410,400);
-    ctx.stroke(); 
-    for(var i =0; i< bufferLength; i++)
+    for(let i =0; i< bufferLength; i++)
     {
         lineHeight = dataArray[i];
-        let r = lineHeight * (250 * (i/bufferLength));
-        let g = 200 * (i/bufferLength);
-        let b = 0;
         ctx.beginPath();
         if(i%2==0){
             lineHeight+=400;
