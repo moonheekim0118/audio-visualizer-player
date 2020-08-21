cfb6f1016190e124e1409e3f94d0805306f0481a
moonee <uuuu118@naver.com>
2020-08-21 20:40:25 +0900

    Edit visualizer style - stroke width, stroke style relative 하게 변경

--- visualizer.js
+++ visualizer.js
@@ -17,21 +17,26 @@ const dataArray=new Uint8Array(bufferLength);
 function renderFrame(){
     requestAnimationFrame(renderFrame);
     let lineHeight;
+    let lineWidth;
     let x = 0;
     const WIDTH = canvas.width;
     const HEIGHT = canvas.height;
     x = WIDTH/7;
     analyser.getByteFrequencyData(dataArray);
-    const grd = ctx.createLinearGradient(0, 0, WIDTH, 0);
+    let grd = ctx.createLinearGradient(0, 0, WIDTH, 0);
     grd.addColorStop(0, "#7204b1");
     grd.addColorStop(1, "#c483ea");
     ctx.fillRect(0,0,WIDTH,HEIGHT);
     ctx.fillStyle = grd;
     ctx.globalAlpha = 0.5;
     let half = bufferLength/2;
-    const gap = x / 20;
-    for(let i = half-1; i>= 0 ; i --){
+    const gap = x / 18;
+    for(let i = half-22; i>= 0 ; i --){
         lineHeight = dataArray[i];
+        lineWidth=( (lineHeight+WIDTH)/bufferLength);
+        const r = 100 * (i/bufferLength);
+        const g = 255;
+        const b =  lineHeight + (250 * (i/bufferLength));
         ctx.beginPath();
         if(i%2==0){
             lineHeight+=400;
@@ -39,22 +44,28 @@ function renderFrame(){
             lineHeight= 400-lineHeight;
         }
         ctx.quadraticCurveTo(x, 400, x, lineHeight)
+        ctx.lineWidth = lineWidth;
         ctx.stroke(); 
-        ctx.strokeStyle = '#fff';
+        ctx.strokeStyle = `rgb(${r},${g},${b})`;
         x+=gap;
     }
-    for(let i =half; i< bufferLength; i++)
+    for(let i =half-21; i< bufferLength; i++)
     {
         lineHeight = dataArray[i];
+        lineWidth=( (lineHeight+WIDTH)/bufferLength);
+        const r = lineHeight + (250 * (i/bufferLength));
+        const g = 255;
+        const b =250 * (i/bufferLength);
         ctx.beginPath();
-        if(i%2!==0){
+        if(i%2==0){
             lineHeight+=400;
         }else{
             lineHeight= 400-lineHeight;
         }
-        ctx.quadraticCurveTo(x, 400, x, lineHeight)
+        ctx.quadraticCurveTo(x, 400, x, lineHeight);
+        ctx.lineWidth = lineWidth;
         ctx.stroke(); 
-        ctx.strokeStyle = '#fff';
+        ctx.strokeStyle =`rgb(${r},${g},${b})`;
         x+=gap;
     }
 
