588f56e752e5e49da4627939a29aea4cb782586d
moonee <uuuu118@naver.com>
2020-08-21 09:10:31 +0900

    Fix visualizer bug - 모바일 버전에서bar 간격이 넓어지는 버그 해결 -> x와 간격의 크기를 relative 하게 변경 - 모바일 버전에서 resize 시 변경되지 않는 visualizer 버그 해결 -> redrawing 함수 추가

--- visualizer.js
+++ visualizer.js
@@ -4,34 +4,32 @@ const src = context.createMediaElementSource(audio);
 const analyser = context.createAnalyser();
 
 let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
-let ctx = canvas.getContext("2d");
-
 src.connect(analyser);
 analyser.connect(context.destination);
 
 analyser.fftSize=256;
 const bufferLength = analyser.frequencyBinCount;
 const dataArray=new Uint8Array(bufferLength);
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
-
-let lineHeight;
-let x = 0;
-
 
 function renderFrame(){
     requestAnimationFrame(renderFrame);
-    x = 200;
+    let lineHeight;
+    let x = 0;
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+    x = WIDTH/7;
     analyser.getByteFrequencyData(dataArray);
-    const grd = ctx.createLinearGradient(0, 0, 1700, 0);
+    const grd = ctx.createLinearGradient(0, 0, WIDTH, 0);
     grd.addColorStop(0, "#7204b1");
     grd.addColorStop(1, "#c483ea");
     ctx.fillRect(0,0,WIDTH,HEIGHT);
     ctx.fillStyle = grd;
     ctx.globalAlpha = 0.5;
     let half = bufferLength/2;
+    const gap = x / 20;
     for(let i = half-1; i>= 0 ; i --){
         lineHeight = dataArray[i];
         ctx.beginPath();
@@ -43,7 +41,7 @@ function renderFrame(){
         ctx.quadraticCurveTo(x, 400, x, lineHeight)
         ctx.stroke(); 
         ctx.strokeStyle = '#fff';
-        x+=12;
+        x+=gap;
     }
     for(let i =half; i< bufferLength; i++)
     {
@@ -57,8 +55,16 @@ function renderFrame(){
         ctx.quadraticCurveTo(x, 400, x, lineHeight)
         ctx.stroke(); 
         ctx.strokeStyle = '#fff';
-        x+=12;
+        x+=gap;
     }
 
 }
+
+const redrawing = function(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    canvas.width = window.innerWidth;
+    canvas.height= window.innerHeight;
+}
 audio.addEventListener('play', renderFrame);
+
+window.addEventListener('resize',redrawing);
